0dcb68f3b47fb4d76c26eab118ea723fed661a54
itamar gueta <itamar.guetta@gmail.com>
2022-11-21 17:06:35 +0200

    commit

--- main.c
+++ main.c
@@ -7,45 +7,45 @@ int main()
     int num2;
     int i;
 
-    printf("Enter two integers: ");
+    //printf("Enter two integers: ");
     scanf("%d", &num1);
     scanf("%d", &num2);
 
     if (num1>=0 && num2>=0)
     {
-            printf("\nThe Armstrong numbers are: ");
+            printf("\nThe Armstrong numbers are:");
             for (i = num1; i <= num2; i++)
             {
                 if(isArmstrong(i) == 1)
                 {
-                    printf("%d ", i);
+                    printf(" %d ", i);
                 }
             }
 
-            printf("\nThe Palindromes numbers are: ");
+            printf("\nThe Palindromes numbers are:");
             for (i = num1; i <= num2; i++)
             {
                     if(isPalindrome(i) == 1)
                 {
-                    printf("%d ", i);
+                    printf(" %d ", i);
                 }
             }
 
-            printf("\nThe Prime numbers are: ");
+            printf("\nThe Prime numbers are:");
             for (i = num1; i <= num2; i++)
             {
                  if(isPrime(i) == 1)
                 {
-                    printf("%d ", i);
+                    printf(" %d ", i);
                 }
             }
 
-            printf("\nThe Strong numbers are: ");
+            printf("\nThe Strong numbers are:");
             for (i = num1; i <= num2; i++)
             {
                  if(isStrong(i) == 1)
                 {
-                    printf("%d ", i);
+                    printf(" %d ", i);
                 }
             }
             printf("\n");
